f36854372947776872ac52b90b01f15979d6aebe
Akash <akash.sagar@distacart.com>
2024-08-01 16:36:40 +0530

    form added on image block

--- assets/custom.js
+++ assets/custom.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
         dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 750,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
