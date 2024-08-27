4ec2935e002fd4e6f474e379ff5d0616344f6dbb
Akash <akash.sagar@distacart.com>
2024-08-27 16:22:33 +0530

    counsaltation form added

--- assets/global.js
+++ assets/global.js
@@ -76,19 +76,13 @@ document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
     summary.setAttribute('aria-controls', summary.nextElementSibling.id);
   }
 
-  const toggleExpanded = (event) => {
+  summary.addEventListener('click', (event) => {
     event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
-  };
-
-  summary.addEventListener('click', toggleExpanded);
-
-  // Add hover event listener to apply the same functionality as click
-  summary.addEventListener('mouseover', toggleExpanded);
+  });
 
   if (summary.closest('header-drawer, menu-drawer')) return;
   summary.parentElement.addEventListener('keyup', onKeyUpEscape);
 });
-
 
 const trapFocusHandlers = {};
 
